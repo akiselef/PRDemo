41af75a558cff1e923bd082ff5286fa6e92264c5
akiselef <rage4ndlove@gmail.com>
2021-02-15 19:11:30 +0400

    add basic test

--- src/test/java/BasicGoogleTest.java
+++ src/test/java/BasicGoogleTest.java
@@ -1,3 +1,16 @@
-public class BasicGoogleTest {
+import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class BasicGoogleTest {
+    @Test
+    void searchInGoogleTest() {
+        open("https://www.google.com/");
+        $(byName("q")).setValue("Selenide");
+        $(byName("btnK")).click();
+        $("#search").shouldHave(text("selenide.org"));
+    }
 }
